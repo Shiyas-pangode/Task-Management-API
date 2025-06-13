f7a58efc9a0a0f2454c96d0db45370aedd9665b8
shiyas-pangode <shiyasschachu@gmail.com>
2025-06-13 15:56:42 +0530

    update

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,9 +4,9 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/',include('api.v1.user.urls')),
-    path('api/auth/',include('api.v1.auth.urls')),
-    path('api/taskmanager/',include('api.v1.taskmanager.urls')),
-    path('api/employee/',include('api.v1.employee.urls')),
+    path('api/v1/user/',include('api.v1.user.urls')),
+    path('api/v1/auth/',include('api.v1.auth.urls')),
+    path('api/v1/taskmanager/',include('api.v1.taskmanager.urls')),
+    path('api/v1/employee/',include('api.v1.employee.urls')),
 
 ]
