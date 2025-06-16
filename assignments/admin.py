4caf72e7d975e8eba0e1a583056b7ae85986821a
shiyas-pangode <shiyasschachu@gmail.com>
2025-06-16 19:20:22 +0530

    update

--- assignments/admin.py
+++ assignments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TaskModel
 
-# Register your models here.
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ('title','description','status')
+admin.site.register(TaskModel ,TaskModelAdmin)
