b1bba88033729defe160bd8d67fe4072021ac2df
shiyas-pangode <shiyasschachu@gmail.com>
2025-06-17 19:41:31 +0530

    update

--- general/models.py
+++ general/models.py
@@ -5,23 +5,26 @@ from datetime import timedelta
 
 class BaseModel(models.Model):
 
-    created_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True,blank=True)
     is_deleted = models.BooleanField(blank=True,default=None)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta :
         abstract = True
 
     
-    def is__deleted(self):
+    def is_deleted(self):
 
         self.is__deleted=True
         self.save()
 
     def soft_delete(self):
         self.is_deleted = True
+        self.deleted_at = timezone.now()
         self.save(update_fields=['is_deleted', 'updated_at'])
 
     def restore(self):
         self.is__deleted=False
-        self.save()
+        self.deleted_at=None
+        self.save(update_fields=['is_deleted', 'deleted_at'])
